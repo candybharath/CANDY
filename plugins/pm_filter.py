94c3aae0afd287f6c2b7cb9624ca035f880b560f
ᴀꜰsᴀʟ ʜᴀsʜɪᴍ​ <123615887+CyberTG@users.noreply.github.com>
2023-04-02 22:04:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -938,7 +938,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "sinfo":
         await query.answer(text=script.SINFO, show_alert=True)
         
-   elif query.data == "rendering_info":
+    elif query.data == "rendering_info":
         await query.answer(text=script.RENDERING_TXT, show_alert=True)
 
     elif query.data == "start":
