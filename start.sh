ed3acc3b86633c8197c9f2dfdd044fa63e9235cf
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-05 01:39:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/azanhelpdesk/Dq-Tom-New.git /Dq-Tom-New
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Dq-Tom-New
 fi
-cd /DQ-The-File-Donor
+cd /Dq-Tom-New
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Dq-Tom-New...."
 python3 bot.py
