0c489b3d8647f9f1105914f1181f6a286738bc71
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-05 14:47:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -508,12 +508,12 @@ async def settings(client, message):
             ],
             [
                 InlineKeyboardButton(
-                    'Fɪʟᴇ Sᴇɴᴅ Mᴏᴅᴇ',
-                    callback_data=f'setgs#bottpm#{settings["bottpm"]}#{grp_id}',
+                    'Pʀᴏᴛᴇᴄᴛ Cᴏɴᴛᴇɴᴛ',
+                    callback_data=f'setgs#file_secure#{settings["file_secure"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    'Mᴀɴᴜᴀʟ Sᴛᴀʀᴛ' if settings["bottpm"] else 'Aᴜᴛᴏ Sᴇɴᴅ',
-                    callback_data=f'setgs#bottpm#{settings["bottpm"]}#{grp_id}',
+                    '✔ Oɴ' if settings["file_secure"] else '✘ Oғғ',
+                    callback_data=f'setgs#file_secure#{settings["file_secure"]}#{grp_id}',
                 ),
             ],
             [
