5548785ca5929545da49f01d21e415e4e923e40f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-05 16:33:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -42,23 +42,55 @@ SPELL_CHECK = {}
 async def give_filter(client, message):
     content = message.text
     searchh = message.text
-    imdb = await get_poster(searchh) if IMDB else None                             
-    if imdb and imdb.get('poster'):
-        try:
-            btn = [[
-                InlineKeyboardButton(f"{imdb.get('title')}", url="imdb['url']")
-            ]]                                      
-            await msg.reply_photo(photo=imdb['poster'],
+    imdb = await get_poster(searchh) if IMDB else None  
+    if imdb:
+            caption = IMDB_TEMPLATE.format(
+                query=searchh,                
+                title=imdb['title'],
+                votes=imdb['votes'],
+                aka=imdb["aka"],
+                seasons=imdb["seasons"],
+                box_office=imdb['box_office'],
+                localized_title=imdb['localized_title'],
+                kind=imdb['kind'],
+                imdb_id=imdb["imdb_id"],
+                cast=imdb["cast"],
+                runtime=imdb["runtime"],
+                countries=imdb["countries"],
+                certificates=imdb["certificates"],
+                languages=imdb["languages"],
+                director=imdb["director"],
+                writer=imdb["writer"],
+                producer=imdb["producer"],
+                composer=imdb["composer"],
+                cinematographer=imdb["cinematographer"],
+                music_team=imdb["music_team"],
+                distributors=imdb["distributors"],
+                release_date=imdb['release_date'],
+                year=imdb['year'],
+                genres=imdb['genres'],
+                poster=imdb['poster'],
+                plot=imdb['plot'],
+                rating=imdb['rating'],
+                url=imdb['url'],
+                **locals()
+            )                          
+        if imdb and imdb.get('poster'):
+            try:
+                btn = [[
+                    InlineKeyboardButton(f"{imdb.get('title')}", url="imdb['url']")
+                ]]                                      
+                await message.reply_photo(photo=imdb['poster'],
                                        reply_markup=InlineKeyboardMarkup(btn))
                     
                                                 
-            except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-                pic = imdb.get('poster')
-                poster = pic.replace('.jpg', "._V1_UX360.jpg")
-                await msg.reply_photo(photo=imdb['poster'], caption=caption,
+                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+                    pic = imdb.get('poster')
+                    poster = pic.replace('.jpg', "._V1_UX360.jpg")
+                    await message.reply_photo(photo=imdb['poster'], caption=caption,
                                             reply_markup=InlineKeyboardMarkup(btn))
-            except Exception as e:
-                logger.exception(e)
+                except Exception as e:
+                    logger.exception(e)
         try:
                     
             if AUTH_CHANNEL and not await is_subscribed(client, message):
