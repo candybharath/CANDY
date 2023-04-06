6c77b1839405b0c140527dcb3a81760d65ab1dc0
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-06 15:45:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -363,12 +363,7 @@ async def languages_cb_handler(client: Client, query: CallbackQuery):
             show_alert=True,
         )        
     _, search, key = query.data.split("#")
-    if int(req) not in [query.from_user.id, 0]:
-#    if int(query.from_user.id) not in [query.from_user.id, 0]:
-        return await query.answer(
-            f"⚠️ ʜᴇʟʟᴏ{query.from_user.first_name},\nᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,\nʀᴇQᴜᴇꜱᴛ ʏᴏᴜʀ'ꜱ...",
-            show_alert=True,
-        )
+
     btn = [
         [
             InlineKeyboardButton(
