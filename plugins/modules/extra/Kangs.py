7a53a53a5de1acf93c73309b366206833e52dc88
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-11 20:20:30 +0530

    Update Kangs.py

--- plugins/modules/extra/Kangs.py
+++ plugins/modules/extra/Kangs.py
@@ -5,7 +5,6 @@ from pyrogram import filters, Client
 from pyrogram.types import *
 from pyrogram.errors import *
 
-from utils.stickerset import *
 
 import math
 import os
@@ -13,7 +12,9 @@ import os
 from PIL import Image
 from pyrogram import Client, raw
 from pyrogram.file_id import FileId
+from typing import List
 
+from pyrogram import Client, errors, raw
     
 MAX_STICKERS = (120)  # would be better if we could fetch this limit directly from telegram
 SUPPORTED_TYPES = ["jpeg", "png", "webp"]
@@ -23,6 +24,61 @@ SUPPORTED_TYPES = ["jpeg", "png", "webp"]
 
 STICKER_DIMENSIONS = (512, 512)
 
+async def get_sticker_set_by_name(
+        client: Client, name: str
+) -> raw.base.messages.StickerSet:
+    try:
+        return await client.send(
+            raw.functions.messages.GetStickerSet(
+                stickerset=raw.types.InputStickerSetShortName(short_name=name),
+                hash=0,
+            )
+        )
+    except errors.exceptions.not_acceptable_406.StickersetInvalid:
+        return None
+
+
+# Known errors: (I don't see a reason to catch them as we, for sure, won't face them right now):
+# errors.exceptions.bad_request_400.PackShortNameInvalid -> pack name needs to end with _by_botname
+# errors.exceptions.bad_request_400.ShortnameOccupyFailed -> pack's name is already in use
+
+
+async def create_sticker_set(
+        client: Client,
+        owner: int,
+        title: str,
+        short_name: str,
+        stickers: List[raw.base.InputStickerSetItem],
+) -> raw.base.messages.StickerSet:
+    return await client.send(
+        raw.functions.stickers.CreateStickerSet(
+            user_id=await client.resolve_peer(owner),
+            title=title,
+            short_name=short_name,
+            stickers=stickers,
+        )
+    )
+
+
+async def add_sticker_to_set(
+        client: Client,
+        stickerset: raw.base.messages.StickerSet,
+        sticker: raw.base.InputStickerSetItem,
+) -> raw.base.messages.StickerSet:
+    return await client.send(
+        raw.functions.stickers.AddStickerToSet(
+            stickerset=raw.types.InputStickerSetShortName(
+                short_name=stickerset.set.short_name
+            ),
+            sticker=sticker,
+        )
+    )
+
+
+async def create_sticker(
+        sticker: raw.base.InputDocument, emoji: str
+) -> raw.base.InputStickerSetItem:
+    return raw.types.InputStickerSetItem(document=sticker, emoji=emoji)
 
 async def resize_file_to_sticker_size(file_path: str) -> str:
     im = Image.open(file_path)
