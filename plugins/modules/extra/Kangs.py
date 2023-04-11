ac58c3ea6598b9cb77dfe0f36166e5956cfd6f59
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-12 00:06:12 +0530

    Update Kangs.py

--- plugins/modules/extra/Kangs.py
+++ plugins/modules/extra/Kangs.py
@@ -173,9 +173,8 @@ async def sticker_image(_, message: Message):
 
 @Client.on_message(filters.command("kang"))
 async def kang(client, message: Message):
-    user_id = message.from_user.id
-    message_id = message.message_id
-    name_format = f"StarkBots_{user_id}_{message_id}"
+    
+    name_format = f"StarkBots"
     BOT_USERNAME = client.username
     if not message.reply_to_message:
         return await message.reply_text("Reply to a sticker/image to kang it.")
