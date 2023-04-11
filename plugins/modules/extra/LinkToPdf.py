2edc6208eb7dcad8552ed8b5a0afe01a175e4c98
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-11 14:42:02 +0530

    Update LinkToPdf.py

--- plugins/modules/extra/LinkToPdf.py
+++ plugins/modules/extra/LinkToPdf.py
@@ -1,92 +1,56 @@
 import os
-import requests
-import weasyprint
-import urllib.request
-from plugins.helpers.presets import Presets
-from bs4 import BeautifulSoup
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from PIL import Image
+from pyrogram import Client,filters 
+from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
 
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-if bool(os.environ.get("ENV", False)):
-    from plugins.helpers.sample_config import Config
-else:
-    from config import Config
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# -------------------------- Bot Configuration ---------------------------------------------- #
-Bot = Client(
-    "link2PdfBot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-)
+API_ID = int(os.environ.get("API_ID", 12345))
 
-
-# ------------------------------ Start Command ---------------------------------------------- #
-
-
-# -------------------------------- Main execution fn --------------------------------------- #
-@Client.on_message(filters.command("linkpdf") & filters.text)
-async def link_extract(self, m: Message):
-    if not m.text.startswith("http"):
-        await m.reply_text(
-            Presets.INVALID_LINK_TXT,
-            reply_to_message_id=m.message_id,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Close", callback_data="close_data")]]
-            )
-        )
-        return
-    file_name = str()
-    #
-    thumb_path = os.path.join(os.getcwd(), "img")
-    if not os.path.isdir(thumb_path):
-        os.makedirs(thumb_path)
-        urllib.request.urlretrieve(Presets.THUMB_URL, os.path.join(thumb_path, "thumbnail.png"))
-    else:
-        pass
-    #
-    thumbnail = os.path.join(os.getcwd(), "img", "thumbnail.png")
-    #
-    await self.send_chat_action(m.chat.id, "typing")
-    msg = await m.reply_text(Presets.PROCESS_TXT, reply_to_message_id=m.message_id)
-    try:
-        req = requests.get(m.text)
-        # using the BeautifulSoup module
-        soup = BeautifulSoup(req.text, 'html.parser')
-        # extracting the title frm the link
-        for title in soup.find_all('title'):
-            file_name = str(title.get_text()) + '.pdf'
-        # Creating the pdf file
-        weasyprint.HTML(m.text).write_pdf(file_name)
-    except Exception:
-        await msg.edit_text(
-            Presets.ERROR_TXT,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Close", callback_data="close_data")]]
-            )
-        )
-        return
-    try:
-        await msg.edit(Presets.UPLOAD_TXT)
-    except Exception:
-        pass
-    await self.send_chat_action(m.chat.id, "upload_document")
-    await m.reply_document(
-        document=file_name,
-        caption=Presets.CAPTION_TXT.format(file_name),
-        thumb=thumbnail
+API_HASH = os.environ.get("API_HASH", "")
+app = Client(
+        "pdf",
+        bot_token=BOT_TOKEN,api_hash=API_HASH,
+            api_id=API_ID
     )
-    print(
-        '@' + m.from_user.username if m.from_user.username else m.from_user.first_name,
-        "has downloaded the file",
-        file_name
-    )
-    try:
-        os.remove(file_name)
-    except Exception:
-        pass
-    await msg.delete()
 
 
-# --------------------------------- Close Button Call Back --------------------------------- #
+LIST = {}
+
+
+
+@Client.on_message(filters.command("imgpdf") & filters.photo)
+async def pdf(client,message):
+ 
+ if not isinstance(LIST.get(message.from_user.id), list):
+   LIST[message.from_user.id] = []
+
+  
+ 
+ file_id = str(message.photo.file_id)
+ ms = await message.reply_text("Converting to PDF ......")
+ file = await client.download_media(file_id)
+ 
+ image = Image.open(file)
+ img = image.convert('RGB')
+ LIST[message.from_user.id].append(img)
+ await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
+ 
+
+@Client.on_message(filters.command(['convert']))
+async def done(client,message):
+ images = LIST.get(message.from_user.id)
+
+ if isinstance(images, list):
+  del LIST[message.from_user.id]
+ if not images:
+  await message.reply_text( "No image !!")
+  return
+
+ path = f"{message.from_user.id}" + ".pdf"
+ images[0].save(path, save_all = True, append_images = images[1:])
+ 
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here your pdf !!")
+ os.remove(path)
