515fd6323d14217ffa6e9fa7bb29adb91b90f03a
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 01:54:24 +0530

    Update ShareText.py

--- plugins/modules/extra/ShareText.py
+++ plugins/modules/extra/ShareText.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from urllib.parse import quote
 # from info import SUPPORT_CHAT
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -10,18 +10,18 @@ def share_link(text: str) -> str:
 @Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
 async def share_text(client, message):
     text = message.text
-    id = message.message_id
+    reply_to_message_id=reply_to_id
     reply = message.reply_to_message.message.text
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     input_split = message.text.split(None, 1)
     if len(input_split) == 2:
         input_text = input_split[1]
-    elif text and (message.text or id.caption):
-        input_text = message.text or id.caption
+    elif text and (message.text or reply_to_message_id.caption):
+        input_text = message.text or reply_to_message_id.caption
     else:
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**")
             
         return
-        await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
+    await message.reply_text(share_link(input_text), reply_to_id=reply_to_message_id)
        
