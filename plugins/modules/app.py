af157c208f33c06be7a1eecfb4bb8d7c1fb8d82a
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-12 15:55:18 +0530

    Update app.py

--- plugins/modules/app.py
+++ plugins/modules/app.py
@@ -14,7 +14,7 @@ Bot = Client(
 
 @Client.on_message(filters.command('app') & filters.text)
 async def video(client, message):
-    app = mesaage.text
+    app = message.text
     results = play_scraper.search(app)
     answers = []
     for result in results:
