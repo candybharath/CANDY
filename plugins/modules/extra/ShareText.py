2a60f5462ba6ecfd38aca0dd93f2e06769c8e703
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 01:59:11 +0530

    Update ShareText.py

--- plugins/modules/extra/ShareText.py
+++ plugins/modules/extra/ShareText.py
@@ -1,34 +1,25 @@
 import os
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from urllib.parse import quote
-# from info import SUPPORT_CHAT
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def share_link(text: str) -> str:
-    return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
-
-@Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
+@Client.on_message(filters.command(["share_text", "share", "sharetext",]))
 async def share_text(client, message):
-    the_real_message = None
-    reply_to_id = None
-
-    if message.reply_to_message:
-        the_real_message = message.reply_to_message
-    else:
-        the_real_message = message
-    text = message.text
-    reply_to_message_id=reply_to_id
-    reply = message.reply_to_message.message.text
-    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    reply = message.reply_to_message
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
     input_split = message.text.split(None, 1)
     if len(input_split) == 2:
         input_text = input_split[1]
-    elif text and (message.text or reply_to_message_id.caption):
-        input_text = message.text or reply_to_message_id.caption
+    elif reply and (reply.text or reply.caption):
+        input_text = reply.text or reply.caption
     else:
         await message.reply_text(
-            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**")
-            
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
+            reply_to_message_id=reply_id,               
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/MKN_BOTZ_DISCUSSION_GROUP")]])
+            )                                                   
         return
-    await message.reply_text(share_link(input_text), reply_to_id=reply_to_message_id)
-       
+    await message.reply_text(
+        text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
+        reply_to_message_id=reply_id,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])
