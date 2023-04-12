762aa15b7769b92653c77f7c04fdfd874b028d02
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 03:06:30 +0530

    Update RemovuBg.py

--- plugins/modules/RemovuBg.py
+++ plugins/modules/RemovuBg.py
@@ -1,145 +1,26 @@
-import os
-import requests
-from dotenv import load_dotenv
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from rembg import remove
+from PIL import Image
+from pathlib import Path
 
+def remove_bg():
+    list_of_extensions = ['*.jpg']
+    all_files = []
 
-load_dotenv()
+    for ext in list_of_extensions:
+        all_files.extend(Path('..folder path/input_imgs').glob(ext))
+    
+    # print(all_files)
+    
+    for index, item in enumerate(all_files):
+        input_path = Path(item)
+        file_name = input_path.stem
 
-REMOVEBG_API = "z6M8qGTPj4fHtgKfvcrxdNTX"
-UNSCREEN_API = "1pWrEXF6pTmNyuMjbgghvbW4"
+        output_path = f'...folder path/output_imgs/{file_name}_output.png'
+        input_img = Image.open(input_path)
+        output_img = remove(input_img)
+        output_img.save(output_path)
 
-Bot = Client(
-    "Remove Background Bot",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
-)
+        print(f'Completed: {index + 1}/{len(all_files)}')
 
-
-
-BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Join Channel', url='https://telegram.me/nasrani_update')
-        ]
-    ]
-)
-
-
-ERROR_BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Help', callback_data='help'),
-            InlineKeyboardButton('Close', callback_data='close')
-        ]
-    ]
-)
-
-
-
-
-
-
-
-@Client.on_message(filters.command("bg") & (filters.photo | filters.video | filters.document))
-async def remove_background(bot, update):
-    if not REMOVEBG_API:
-        await update.reply_text(
-            text="Error :- Remove BG Api is error",
-            quote=True,
-            disable_web_page_preview=True,
-            reply_markup=ERROR_BUTTONS
-        )
-        return
-    await update.reply_chat_action(enums.ChatAction.TYPING)
-    message = await update.reply_text(
-        text="Processing",
-        quote=True,
-        disable_web_page_preview=True
-    )
-    try:
-        new_file_name = f"./{str(update.from_user.id)}"
-        if update.photo or (
-            update.document and "image" in update.document.mime_type
-        ):
-            new_file_name += ".png"
-            file = await update.download()
-            await message.edit_text(
-                text="Photo downloaded successfully. Now removing background.",
-                disable_web_page_preview=True
-            )
-            new_document = removebg_image(file)
-        elif update.video or (
-            update.document and "video" in update.document.mime_type
-        ):
-            new_file_name += ".webm"
-            file = await update.download()
-            await message.edit_text(
-                text="Video downloaded successfully. Now removing background.",
-                disable_web_page_preview=True
-            )
-            new_document = removebg_video(file)
-        else:
-            await message.edit_text(
-                text="Media not supported",
-                disable_web_page_preview=True,
-                reply_markup=ERROR_BUTTONS
-            )
-        try:
-            os.remove(file)
-        except:
-            pass
-    except Exception as error:
-        await message.edit_text(
-            text=error,
-            disable_web_page_preview=True
-        )
-    try:
-        with open(new_file_name, "wb") as file:
-            file.write(new_document.content)
-        await update.reply_chat_action("upload_document")
-    except Exception as error:
-        await message.edit_text(
-           text=error,
-           reply_markup=ERROR_BUTTONS
-        )
-        return
-    try:
-        await update.reply_document(
-            document=new_file_name,
-            quote=True
-        )
-        try:
-            os.remove(file)
-        except:
-            pass
-    except Exception as error:
-        await message.edit_text(
-            text=f"Error:- `{error}`",
-            disable_web_page_preview=True,
-            reply_markup=ERROR_BUTTONS
-        )
-
-
-def removebg_image(file):
-    return requests.post(
-        "https://api.remove.bg/v1.0/removebg",
-        files={"image_file": open(file, "rb")},
-        data={"size": "auto"},
-        headers={"X-Api-Key": REMOVEBG_API}
-    )
-
-
-def removebg_video(file):
-    return requests.post(
-        "https://api.unscreen.com/v1.0/videos",
-        files={"video_file": open(file, "rb")},
-        headers={"X-Api-Key": UNSCREEN_API}
-    )
-
-
-
-
-
+def main():
+    remove_bg()
