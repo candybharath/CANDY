69cc13a9baa7f247a4eb30d2712257887eba8521
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-12 13:34:02 +0530

    Update Trans.py

--- plugins/modules/extra/Trans.py
+++ plugins/modules/extra/Trans.py
@@ -1,96 +1,170 @@
-import sys
-import traceback
-from functools import wraps
-
-from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from info import AUTH_CHANNEL
-
-
-
-
-
-
+import asyncio
+from os import environ
+from plugins.helpers.extra import MALIKK, MALK
 from googletrans import Translator
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from plugins.helpers.list import list
-from plugins.helpers.database import find_one
+from pyrogram import Client, filters, enums 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-from pyrogram import enums, filters
+@Client.on_message(filters.command(["tr"]))
+async def left(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			hehek = InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton(
+                                            "Update Channel", url='https://t.me/m_house786'
+                                        ),
+                                    ],
+				    [
+                                        InlineKeyboardButton(
+                                            "𝘊𝘭𝘰𝘴𝘦", callback_data="close_data"
+                                        )
+                                    ],
+                                ]
+                            )
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
+				await message.reply_text(f"translated from {fromt.capitalize()} to {to.capitalize()}\n\n```{translation.text}```", reply_markup=hehek, quote=True)
+			except:
+			   	await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```", reply_markup=hehek, quote=True)
+			
 
+		except :
+			print("error")
+	else:
+	                 m = await message.reply_photo(
+                         photo=(MALK),
+                         caption=(MALIKK.format(message.from_user.mention)),
+                         reply_markup=InlineKeyboardMarkup(
+                                   [[
+                                     InlineKeyboardButton('Close', callback_data="close_data"),
+                                                                         
+                                   ]]
+                         ),
+                         parse_mode=enums.ParseMode.HTML
+) 
 
+                     
 
-
-def split_limits(text):
-    if len(text) < 2048:
-        return [text]
-
-    lines = text.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < 2048:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
-    else:
-        result.append(small_msg)
-
-    return result
-
-
-def capture_err(func):
-    @wraps(func)
-    async def capture(client, message, *args, **kwargs):
-        try:
-            return await func(client, message, *args, **kwargs)
-        except ChatWriteForbidden:
-            await app.leave_chat(message.chat.id)
-            return
-        except Exception as err:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            error_feedback = split_limits(
-                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
-                    0 if not message.from_user else message.from_user.id,
-                    0 if not message.chat else message.chat.id,
-                    message.text or message.caption,
-                    "".join(errors),
-                ),
-            )
-            for x in error_feedback:
-                await app.send_message(AUTH_CHANNEL, x)
-            raise err
-
-    return capture
+#list py   
 
 
-
-
-
-@Client.on_message(filters.command("t"))
-@capture_err
-async def tr(_, message):
-    if len(message.command) != 2:
-        return await message.reply_text("/tr [LANGUAGE_CODE]")
-    lang = message.text.split(None, 1)[1]
-    if not message.reply_to_message or not lang:
-        return await message.reply_text(
-            "Reply to a message with /tr [language code]"
-            + "\nGet supported language list from here -"
-            + " https://py-googletrans.readthedocs.io/en"
-            + "/latest/#googletrans-languages"
-        )
-    reply = message.reply_to_message
-    text = reply.text or reply.caption
-    if not text:
-        return await message.reply_text("Reply to a text to translate it")
-    result = await arq.translate(text, lang)
-    if not result.ok:
-        return await message.reply_text(result.result)
-    await message.reply_text(result.result.translatedText)
+list = {
+"afrikaans":"af",
+"albanian":"sq",
+"amharic":"am",
+"arabic":"ar",
+"armenian":"hy",
+"azerbaijani":"az",
+"basque":"eu",
+"belarusian":"be",
+"bengali":"bn",
+"bosnian":"bs",
+"bulgarian":"bg",
+"catalan":"ca",
+"cebuano":"ceb",
+"chinese": "zh",
+"corsican":"co",
+"croatian":"hr",
+"czech":"cs",
+"danish":"da",
+"dutch":"nl",
+"english":"en",
+"esperanto":"eo",
+"estonian":"et",
+"finnish":"fi",
+"french":"fr",
+"frisian":"fy",
+"galician":"gl",
+"georgian":"ka",
+"german":"de",
+"greek":"el",
+"gujarati":"gu",
+"haitian creole":"ht",
+"hausa":"ha",
+"hawaiian":"haw",
+"hebrew":"he",
+"hindi":"hi",
+"hmong":"hmn",
+"hungarian":"hu",
+"icelandic":"is",
+"igbo":"ig",
+"indonesian":"id",
+"irish":"ga",
+"italian":"it",
+"japanese":"ja",
+"javanese":"jv",
+"kannada":"kn",
+"kazakh":"kk",
+"khmer":"km",
+"kinyarwanda":"rw",
+"korean":"ko",
+"kurdish":"ku",
+"kyrgyz":"ky",
+"lao":"lo",
+"latin":"la",
+"latvian":"lv",
+"lithuanian":"lt",
+"luxembourgish":"lb",
+"macedonian":"mk",
+"malagasy":"mg",
+"malay":"ms",
+"malayalam":"ml",
+"maltese":"mt",
+"maori":"mi",
+"marathi":"mr",
+"mongolian":"mn",
+"myanmar":"my",
+"nepali":"ne",
+"norwegian":"no",
+"nyanja":"ny",
+"odia":"or",
+"pashto":"ps",
+"persian":"fa",
+"polish":"pl",
+"portuguese":"pt",
+"punjabi":"pa",
+"romanian":"ro",
+"russian":"ru",
+"samoan":"sm",
+"scots gaelic":"gd",
+"serbian":"sr",
+"sesotho":"st",
+"shona":"sn",
+"sindhi":"sd",
+"sinhala":"si",
+"slovak":"sk",
+"slovenian":"sl",
+"somali":"so",
+"spanish":"es",
+"sundanese":"su",
+"swahili":"sw",
+"swedish":"sv",
+"tagalog":"tl",
+"tajik":"tg",
+"tamil":"ta",
+"tatar":"tt",
+"telugu":"te",
+"thai":"th",
+"turkish":"tr",
+"turkmen":"tk",
+"ukrainian":"uk",
+"urdu":"ur",
+"uyghur":"ug",
+"uzbek":"uz",
+"vietnamese":"vi",
+"welsh":"cy",
+"xhosa":"xh",
+"yiddish":"yi",
+"yoruba":"yo",
+"zulu":"zu"}
