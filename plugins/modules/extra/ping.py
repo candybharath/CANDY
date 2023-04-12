747ebfe5761e1fcd27babccdbad6cba22ca6a65f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-12 23:06:00 +0530

    Update ping.py

--- plugins/modules/extra/ping.py
+++ plugins/modules/extra/ping.py
@@ -5,7 +5,7 @@ import time
 import random
 from pyrogram import Client, filters
 # from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.helpers.cust_p_filters import f_onw_fliter
 
 
 COMMAND_HAND_LER = "/"
