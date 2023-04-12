56248019543cb91f2f4176d3e8d1b1a62da1f3c4
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 00:04:44 +0530

    Update friday.py

--- plugins/helpers/friday.py
+++ plugins/helpers/friday.py
@@ -150,11 +150,14 @@ async def fetch_audio(client, message):
 
 async def edit_or_reply(message, text, parse_mode=enums.ParseMode.MARKDOWN):
     if message.from_user.id:
-        if message.reply_to_message:
+        if message.reply_to_message.text:
+            text = message.text
             kk = message.reply_to_message.id
             return await message.reply_text(
-#                text, reply_to_message.id=kk, parse_mode=enums.ParseMode.MARKDOWN
-                text, reply_to_message.id=kk
+#                text=message.text,
+                 reply_to_message.id=kk,
+                 parse_mode=enums.ParseMode.MARKDOWN
+                text, reply_to_message.id=kk, parse_mode=enums.ParseMode.MARKDOWN
             )
                 
             
