25a68ca3cf214a5d50791393cc8bf4ff644c1384
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 02:02:33 +0530

    Update ShareText.py

--- plugins/modules/extra/ShareText.py
+++ plugins/modules/extra/ShareText.py
@@ -22,4 +22,4 @@ async def share_text(client, message):
     await message.reply_text(
         text=f"**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(input_text),
         reply_to_message_id=reply_id,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]]))
