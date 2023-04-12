ca3d1e1c49e83a1b2b55009bae007ac797864792
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-12 13:20:28 +0530

    Create Trans.py

---
+++ plugins/modules/extra/Trans.py
@@ -0,0 +1,96 @@
+import sys
+import traceback
+from functools import wraps
+
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+form info import AUTH_CHANNEL
+
+
+
+
+
+
+import re
+import secrets
+
+import subprocess
+from asyncio import Lock
+from re import findall
+
+from pyrogram import enums, filters
+
+
+
+
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+
+    return result
+
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    "".join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await app.send_message(AUTH_CHANNEL, x)
+            raise err
+
+    return capture
+
+
+
+
+
+@Client.on_message(filters.command("t"))
+async def tr(_, message):
+    if len(message.command) != 2:
+        return await message.reply_text("/tr [LANGUAGE_CODE]")
+    lang = message.text.split(None, 1)[1]
+    if not message.reply_to_message or not lang:
+        return await message.reply_text(
+            "Reply to a message with /tr [language code]"
+            + "\nGet supported language list from here -"
+            + " https://py-googletrans.readthedocs.io/en"
+            + "/latest/#googletrans-languages"
+        )
+    reply = message.reply_to_message
+    text = reply.text or reply.caption
+    if not text:
+        return await message.reply_text("Reply to a text to translate it")
+    result = await arq.translate(text, lang)
+    if not result.ok:
+        return await message.reply_text(result.result)
+    await message.reply_text(result.result.translatedText)
