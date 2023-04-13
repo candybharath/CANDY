08c1016ad23748c49867a74e2a51eb0d7cc82eaa
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 12:29:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1014,17 +1014,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
             if imdb and imdb.get('poster'):
                 try:
-                    await query.answer(f"{imdb.get('title')}", show_alert=True)
+                    await query.answer(f"{searchh}", show_alert=True)
                 except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
                     pic = imdb.get('poster')
                     poster = pic.replace('.jpg', "._V1_UX360.jpg")
-                    await query.answer(f"{imdb.get('title')}", show_alert=True)
+                    await query.answer(f"{searchh}", show_alert=True)
                 except Exception as e:
                     logger.exception(e)
-                    await query.answer(f"{imdb.get('title')}", show_alert=True)
+                    await query.answer(f"{searchh}", show_alert=True)
                     await query.message.delete()
                 else:
-                    await query.answer(f"{imdb.get('title')}", show_alert=True)
+                    await query.answer(f"{searchh}", show_alert=True)
                     await query.answer()
 
 
