2960d32bcc2a85a4e5514fd3b9e2a508964dfe77
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 18:06:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -992,7 +992,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         caption = IMDB_TEMPLATE.format(
             query=searchh,
 #            title=imdb.get['title'],imdb.get('title')
-            title=imdb.get('title')
+            title=imdb.get('title'),
             votes = imdb['votes'],
             aka = imdb["aka"],
             seasons = imdb["seasons"],
