5b1b16e1c0b74db564c755ab549c396456504e46
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-13 21:23:17 +0530

    Update rules.py

--- plugins/Admins/rules.py
+++ plugins/Admins/rules.py
@@ -118,7 +118,7 @@ async def r_message(client, message):
 
 
 
-@Client.on_message(filters.private & filters.command("new") & filters.user(ADMINS))             
+@Client.on_message(filters.private & filters.command("new") & filters.reply & filters.user(ADMINS))             
 async def start_message(client, message):    
     searchh = message.text 
 # @Client.on_message(filters.private & filters.forwarded)
